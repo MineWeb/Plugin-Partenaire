cc076c0e87b898d3b05300e32c39a924e4fd722b
StanByes <73603926+StanByes@users.noreply.github.com>
2021-02-15 15:53:19 +0100

    Add Discord link

--- SQL/schema.php
+++ SQL/schema.php
@@ -10,13 +10,14 @@ class PartenaireAppSchema extends CakeSchema {
     public function after($event = array()) {
     }
 
-	public $partenaire__partenaire = array(
+    public $partenaire__partenaire = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
 		'channel' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 250, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'pseudo' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 250, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'type' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 10, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'desc' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 450, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+        	'link' => array('type' => 'string', 'null' => true, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1)
 		),
